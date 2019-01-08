3a082152decdcb3dedf9728542faa3e4e58e3e4c
hophanluan123 <46487933+hophanluan123@users.noreply.github.com>
2019-01-08 14:13:11 +0000

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+ 
+int main() 
+{
+	int kyVong, tienGui, soNam = 0;
+	float laiSuat;
+	cin >> tienGui>>laiSuat >> kyVong;
+	// Lãi don kyVong = tienGui*(1+laiSuat*soNam)
+	cout << ceil((float(kyVong)/tienGui - 1.0)/laiSuat) << endl;
+ 
+	// Lãi kép kyVong = tienGui*(1+laiSuat)^soNam
+	cout << ceil(log(float(kyVong)/tienGui)/log(1.0 + laiSuat)) << endl;
+ 
+	return 0;
+}
