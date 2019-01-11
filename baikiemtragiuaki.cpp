ac2cd658c650f771709853ff5268ce3954b8d9e7
hophanluan123 <46487933+hophanluan123@users.noreply.github.com>
2019-01-11 14:30:49 +0000

    Add files via upload

---
+++ baikiemtragiuaki.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main ()
+{
+	double tienvon;
+	double tienkivong;
+	double nam;
+	double laisuat;
+	double a;
+	double b;
+cout <<" Nhap vao so tien von:";
+cin>>tienvon;
+cout <<"Nhap vao lai suat:";
+cin>>laisuat;
+cout <<"Nhap vao so tien ki vong:";
+cin>>tienkivong;
+laisuat= double ( laisuat / 100);
+a=double(tienkivong /tievon);
+b=double(1+laisuat);
+ if ( tienkivong> tienvon);
+ nam=double(lof(a)/log(b));
+ cout <<"So nam can thiet de dat so tien ki vong:"<<nam;
+  else
+cout <<"Khong hop le.";
+return 0;
+}
